1aedda73169f1b98e4ece34cd82ca11579106d6d
hdfsyu <viyaanchauhan@hotmail.com>
2022-10-02 13:17:19 -0400

    prints the alphabet

--- boot.asm
+++ boot.asm
@@ -1,12 +1,18 @@
-int 0x10
-
-loop:
-    inc al
-    cmp al, 'Z' + 1
-    je exit
+print_alphabet:
+    mov al, 64
+    mov ah, 0xE
+    mov bl, 96
+    mov cl, al
+print_letters:
+    inc cl
+    mov al, cl
     int 0x10
-    jmp loop
-exit:
+    inc bl
+    mov al, bl
+    int 0x10
+    cmp bl, 'z'
+    jne print_letters
     jmp $
+jmp print_alphabet
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55, 0xaa 
