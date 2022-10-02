ba3d87d02664f6156368cb647eb10b39cf4ae844
hdfsyu <viyaanchauhan@hotmail.com>
2022-10-02 18:49:12 -0400

    major rehaul moment

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # The VynOS Kernel rewrite
 buggy unstable kernel rewrite, im not gonna fill this readme with text rn im lazy so just run the file run.sh using the command `./run.sh` you need qemu and nasm someone please make this a better readme
 # Updates
-* we got some keyboard input now, but you can only type 33 characters
+* ~we got some keyboard input now, but you can only type 33 characters~ it broke, so now no input .-.
 * we got github actions so now i dont have to keep on spamming nasm and qemu with commands and my laptop can have a lil rest (yes im compiling and building and committing files all on a slow laptop because my main pc is running windows and i need linux for this)
 # Bugs and other stuff
 so apparently when you type something, if you hit enter (before you hit the limit of characters) the characters you type after pressing enter will overwrite the text you already typed before you hit enter (it will also overwrite the > text) this might be a bug, but i like it so im gonna keep it in there (unless someone decides to make a PR fixing that bug)
--- boot.asm
+++ boot.asm
@@ -1,10 +1,45 @@
 ; so you might be thinking, why the hell is there so many commented out code? well if you uncomment the code below then when you type a character the character will only show up if you press enter.
 [org 0x7c00]
-mov ah, 0x0e
-mov al, '>'
-int 0x10
 mov bp, 0x8000
 mov sp, bp
+mov [disk], dl
+CODE_SEG equ code_descriptor - GDT_Start
+DATA_SEG equ data_descriptor - GDT_Start
+cli
+lgdt [GDT_Descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+jmp $
+GDT_Start:
+    null_descriptor:
+        dd 0
+        dd 0
+    code_descriptor:
+        dw 0xffff
+        dw 0
+        db 0
+        db 0b10011010
+        db 0b11001111
+        db 0
+    data_descriptor:
+        dw 0xffff
+        dw 0
+        db 0
+        db 0b10010010
+        db 0b11001111
+        db 0
+GDT_End:
+GDT_Descriptor:
+    dw GDT_End - GDT_Start - 1
+    dd GDT_Start
+[bits 32]
+start_protected_mode:
+    mov al, 'V'
+    mov ah, 0x0f
+    mov [0xb8000], ax
+disk: db 0
 ;inputLabel:
     ;mov ax, 0
     ;int 0x16
@@ -14,27 +49,28 @@ mov sp, bp
     ;je print
     ;jmp inputLabel
 ;print:
-    ;pop bx TODO: maybe change pop bx to sub bp, 2 mov al, [bp]
+    ;pop bx ;TODO: maybe change pop bx to sub bp, 2 mov al, [bp]
     ;mov ah, 0x0e
     ;mov al, bh
     ;int 0x10
     ;cmp bp, sp
     ;je end
     ;jmp print
-keyboardInput:
-    cmp bx, 33
-    je end
-    mov ah, 0
-    int 0x16
-    mov ah, 0x0e
-    mov [bx], al
-    int 0x10
-    inc bx
-    jmp keyboardInput
 end:
     jmp $
-buffer:
-    times 10 db 0
-    mov bx, buffer
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xaa55
+; some code that i added, just for later (disk reading code)
+times 512 db 'H'
+mov ah, 2
+mov al, 1
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, [disk]
+mov es, bx
+mov bx, 0x7e00
+int 0x13
+mov ah, 0x0e
+mov al, [0x7e00]
+int 0x10
